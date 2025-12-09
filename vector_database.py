606c46a8afff895f0cfecb1c6e70853ee27ba456
VISHI MALIK <150807099+VishiMalik@users.noreply.github.com>
2025-12-09 22:48:51 +0530

    Update vector_database.py

--- vector_database.py
+++ vector_database.py
@@ -1,39 +1,48 @@
-import os
-import pdfplumber
 from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_groq import GroqEmbeddings
 
-# Directory where pdfs are saved
-PDF_DIR = "pdfs/"
+#Step 1: Upload & Load raw PDF(s)
 
-# ---------------- Step 1: Save uploaded PDF ----------------
-def save_pdf(uploaded_file):
-    file_path = os.path.join(PDF_DIR, uploaded_file.name)
-    with open(file_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return file_path
+pdfs_directory = 'pdfs/'
 
-# ---------------- Step 2: Load PDFs ----------------
+def upload_pdf(file):
+    with open(pdfs_directory + file.name, "wb") as f:
+        f.write(file.getbuffer())
+
+
 def load_pdf(file_path):
     loader = PDFPlumberLoader(file_path)
-    return loader.load()
+    documents = loader.load()
+    return documents
 
-# ---------------- Step 3: Split into chunks ----------------
-def create_chunks(documents):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=200
+
+file_path = 'pdfs/universal_decl.pdf'
+documents = load_pdf(file_path)
+#print("PDF pages: ",len(documents))
+
+#Step 2: Create Chunks
+def create_chunks(documents): 
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        add_start_index = True
     )
-    return splitter.split_documents(documents)
+    text_chunks = text_splitter.split_documents(documents)
+    return text_chunks
 
-# ---------------- Step 4: Embeddings (Groq safe) ----------------
-def get_embedding_model():
-    return GroqEmbeddings(model="text-embedding-3-large")
+text_chunks = create_chunks(documents)
+#print("Chunks count: ", len(text_chunks))
 
-# ---------------- Step 5: Build Vectorstore (FAISS) ----------------
-def build_faiss_db(chunks):
-    embeddings = get_embedding_model()
-    db = FAISS.from_documents(chunks, embeddings)
-    return db
+
+#Step 3: Setup Embeddings Model (Use DeepSeek R1 with Ollama)
+ollama_model_name="deepseek-r1:1.5b"
+def get_embedding_model(ollama_model_name):
+    embeddings = OllamaEmbeddings(model=ollama_model_name)
+    return embeddings
+
+#Step 4: Index Documents **Store embeddings in FAISS (vector store)
+FAISS_DB_PATH="vectorstore/db_faiss"
+faiss_db=FAISS.from_documents(text_chunks, get_embedding_model(ollama_model_name))
+faiss_db.save_local(FAISS_DB_PATH)
